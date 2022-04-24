5ea7c5789d46c3573ee90bdf7081ac6c94cf5961
Sherzod7709 <eshnazarov044@gmail.com>
2022-04-24 11:51:22 +0500

    end

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    screens : {
+      ph:'360px',
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1200px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
